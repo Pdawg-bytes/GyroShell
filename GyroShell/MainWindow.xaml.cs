9f2ab03aeb364a15b706219b33bdc74a23ef28c9
Pdawg-bytes <pdawg11239linusfan@gmail.com>
2022-12-15 22:16:28 -0500

    Add source

---
+++ GyroShell/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.WindowManagement;
+using AppWindow = Microsoft.UI.Windowing.AppWindow;
+using Windows.Graphics;
+using Windows.ApplicationModel.Activation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GyroShell
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        AppWindow m_appWindow;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            // Removes titlebar
+            var presenter = GetAppWindowAndPresenter();
+            presenter.IsMaximizable = false;
+            presenter.IsMinimizable = false;
+            presenter.IsAlwaysOnTop = true;
+            presenter.IsResizable = false;
+            presenter.SetBorderAndTitleBar(false, false);
+            m_appWindow = GetAppWindowForCurrentWindow();
+            m_appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+            m_appWindow.Show();
+
+            // Resize Window
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+            int ScreenWidth = (int)Bounds.Width;
+            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = ScreenWidth, Height = 40 });
+        }
+
+        private OverlappedPresenter GetAppWindowAndPresenter()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId WndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            var _apw = AppWindow.GetFromWindowId(WndId);
+
+            return _apw.Presenter as OverlappedPresenter;
+        }
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+
+            return AppWindow.GetFromWindowId(myWndId);
+        }
+
+        private void SwitchPresenter_FullScreen(object sender, RoutedEventArgs e)
+        {
+            m_appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+        }
+    }
+}
