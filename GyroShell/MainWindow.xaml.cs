4e39ea9ef24768fa2ef0e6a69019713f3db3c469
Pdawg-bytes <pdawg11239linusfan@gmail.com>
2022-12-16 23:56:34 -0500

    lots of updates

--- GyroShell/MainWindow.xaml.cs
+++ GyroShell/MainWindow.xaml.cs
@@ -13,23 +13,36 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
+using WinRT;
 using Windows.Foundation.Collections;
 using Windows.UI.WindowManagement;
 using AppWindow = Microsoft.UI.Windowing.AppWindow;
 using Windows.Graphics;
 using Windows.ApplicationModel.Activation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Composition.SystemBackdrops;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using ShowAndHideTaskbar;
+using System.Threading;
+using System.Timers;
+using System.Linq.Expressions;
+using Microsoft.Win32;
+using Windows.UI.Core;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GyroShell
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         AppWindow m_appWindow;
+
+        [DllImport("User32.dll")]
+        public static extern int keybd_event(Byte bVk, Byte bScan, long dwFlags, long dwExtraInfo);
+        public const byte UP = 2;
+        public const byte CTRL = 17;
+        public const byte ESC = 27;
+        public const byte WIN = 91;
+        public const byte A = 65;
 
         public MainWindow()
         {
@@ -41,7 +54,7 @@ namespace GyroShell
             presenter.IsMinimizable = false;
             presenter.IsAlwaysOnTop = true;
             presenter.IsResizable = false;
-            presenter.SetBorderAndTitleBar(false, false);
+            presenter.SetBorderAndTitleBar(true, false);
             m_appWindow = GetAppWindowForCurrentWindow();
             m_appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
             m_appWindow.Show();
@@ -52,7 +65,12 @@ namespace GyroShell
             var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
 
             int ScreenWidth = (int)Bounds.Width;
-            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = ScreenWidth, Height = 40 });
+            appWindow.Resize(new SizeInt32 { Width = ScreenWidth, Height = 50 });
+
+            // TaskbarManager.ShowTaskbar();
+
+            // Init stuff
+            TimeAndDate();
         }
 
         private OverlappedPresenter GetAppWindowAndPresenter()
@@ -60,20 +78,76 @@ namespace GyroShell
             var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
             WindowId WndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
             var _apw = AppWindow.GetFromWindowId(WndId);
+            var handleObject = WinRT.Interop.WindowNative.GetWindowHandle(this);
 
             return _apw.Presenter as OverlappedPresenter;
         }
         private AppWindow GetAppWindowForCurrentWindow()
         {
-            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            IntPtr hWndApp = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId WndIdApp = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWndApp);
 
-            return AppWindow.GetFromWindowId(myWndId);
+            return AppWindow.GetFromWindowId(WndIdApp);
         }
-
-        private void SwitchPresenter_FullScreen(object sender, RoutedEventArgs e)
+        private void TimeAndDate()
         {
-            m_appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);
+            DispatcherTimer dateTimeUpdate = new DispatcherTimer();
+            dateTimeUpdate.Interval = new TimeSpan(1000);
+            dateTimeUpdate.Tick += UpdateMethod;
+            dateTimeUpdate.Start();
         }
-    }
+        private void UpdateMethod(object sender, object e)
+        {
+            TimeText.Text = DateTime.Now.ToString("t");
+            DateText.Text = DateTime.Now.ToString("M");
+        }
+
+        private void SystemControls_Click(object sender, RoutedEventArgs e)
+        {
+            if (SystemControls.IsChecked == true)
+            {
+                keybd_event(WIN, 0, 0, 0);
+                keybd_event(A, 0, 0, 0);
+
+                keybd_event(WIN, 0, UP, 0);
+                keybd_event(A, 0, UP, 0);
+            }
+            else
+            {
+                keybd_event(ESC, 0, 0, 0);
+                keybd_event(ESC, 0, UP, 0);
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (StartButton.IsChecked == true)
+            {
+                keybd_event(CTRL, 0, 0, 0);
+                keybd_event(ESC, 0, 0, 0);
+
+                keybd_event(CTRL, 0, UP, 0);
+                keybd_event(ESC, 0, UP, 0);
+            }
+            else
+            {
+                keybd_event(ESC, 0, 0, 0);
+                keybd_event(ESC, 0, UP, 0);
+            }
+        }
+
+        private async void ActionCenter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var uri = new Uri("ms-actioncenter://");
+                var success = await Windows.System.Launcher.LaunchUriAsync(uri);
+            }
+            catch (Exception NotifURI)
+            {
+                Debug.WriteLine(NotifURI.ToString());
+            }
+        }
+    } 
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# GyroShell
-A shell for Windows 11 (and maybe 10) that aims to create a much nicer and more functional taskbar. Fully written in C# and WASDK.
