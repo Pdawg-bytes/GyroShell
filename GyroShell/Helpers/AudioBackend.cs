7bd55c5723680d036a578ce8e1be3857e78cb4f8
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-03-02 18:11:15 -0500

    some more work on audio

---
+++ GyroShell/Helpers/AudioBackend.cs
@@ -0,0 +1,33 @@
+﻿using CoreAudio;
+using System;
+
+namespace GyroShell.Helpers
+{
+    public class AudioBackend
+    {
+        public enum VolumeUnit
+        {
+            Decibel,
+            Scalar
+        };
+        /*public static float GetSystemVolume(VolumeUnit vUnit)
+        {
+            Guid guid = typeof(MMDeviceEnumerator).GUID;
+            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator(guid);
+            MMDevice defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(CoreAudio.EDataFlow.eRender, CoreAudio.ERole.eMultimedia);
+
+            float currentVolume = 0;
+
+            if (vUnit == VolumeUnit.Decibel)
+            {
+                currentVolume = (float)defaultDevice.AudioEndpointVolume.MasterVolumeLevel;
+            }
+            else if (vUnit == VolumeUnit.Scalar)
+            {
+                currentVolume = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
+            }
+
+            return currentVolume;
+        }*/
+    }
+}
