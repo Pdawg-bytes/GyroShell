0821dfd96c677708314c41fddbde38f9ef64d5e8
Pdawg11239 <83825746+Pdawg-bytes@users.noreply.github.com>
2023-07-18 15:21:39 -0400

    [GyroShell -> Icons] Remove old code

--- GyroShell/Helpers/Win32/GetHandleIcon.cs
+++ GyroShell/Helpers/Win32/GetHandleIcon.cs
@@ -1,72 +1,12 @@
-<<<<<<< Updated upstream
-﻿using Microsoft.UI.Xaml.Media.Imaging;
 using System;
-using System.Diagnostics;
 using System.Drawing;
+using System.Diagnostics;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Graphics.Imaging;
-
-using static GyroShell.Helpers.Win32.Win32Interop;
-
-namespace GyroShell.Helpers.Win32
-{
-    internal class GetHandleIcon
-    {
-        internal static Icon GetIcon(IntPtr hwnd)
-        {
-            GetWindowThreadProcessId(hwnd, out uint pid);
-
-            Process proc = Process.GetProcessById((int)pid);
-
-            return Icon.ExtractAssociatedIcon(proc.MainModule.FileName);
-        }
-
-        public static SoftwareBitmapSource GetWinUI3BitmapSourceFromHIcon(Icon icon)
-        {
-            if (icon == null)
-                return null;
-
-            // convert to bitmap
-            using var bmp = icon.ToBitmap();
-            return GetWinUI3BitmapSourceFromGdiBitmap(bmp);
-        }
-
-        public static SoftwareBitmapSource GetWinUI3BitmapSourceFromGdiBitmap(Bitmap bmp)
-        {
-            if (bmp == null)
-                return null;
-
-            // get pixels as an array of bytes
-            var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
-            var bytes = new byte[data.Stride * data.Height];
-
-            Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
-            bmp.UnlockBits(data);
-
-            // get WinRT SoftwareBitmap
-            var softwareBitmap = new SoftwareBitmap(BitmapPixelFormat.Bgra8, bmp.Width, bmp.Height, BitmapAlphaMode.Premultiplied);
-            softwareBitmap.CopyFromBuffer(bytes.AsBuffer());
-
-            // build WinUI3 SoftwareBitmapSource
-            var source = new SoftwareBitmapSource();
-            source.SetBitmapAsync(softwareBitmap);
-
-            return source;
-        }
-    }
-}
-=======
+using System.Runtime.InteropServices;
 ﻿using Microsoft.UI.Xaml.Media.Imaging;
-using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Graphics.Imaging;
 using static GyroShell.Helpers.Win32.Win32Interop;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace GyroShell.Helpers.Win32
 {
@@ -139,4 +79,3 @@ namespace GyroShell.Helpers.Win32
         }
     }
 }
->>>>>>> Stashed changes
